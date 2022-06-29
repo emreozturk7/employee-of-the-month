593ea4fae9908785231ae63afa9f1b1c1ffc8d7f
emreozturk <emre_oztrk_99@hotmail.com>
2022-06-29 21:00:25 +0300

    Detail page tasarımı tamamlandı.

--- src/components/DetailCard/DetailCard.js
+++ src/components/DetailCard/DetailCard.js
@@ -4,29 +4,30 @@ import './styles/DetailCard.sass';
 function DetailCard() {
     return (
         <div>
-            <div class="detail-container">
-                <div class="card-container flex-column">
-                    <div class="user-info flex-column">
-                        <img src="https://robohash.org/hicveldicta.png" alt="" class="user-photo" />
-                        <div class="name-age flex-row">
-                            <p class="name">Victor Crest</p>
-                            <p class="age">26</p>
+            <div className="detail-container">
+                <div className="card-container flex-column">
+                    <div className="user-info flex-column">
+                        <img src="https://robohash.org/hicveldicta.png" alt="" className="user-photo" />
+                        <div className="name-age flex-row">
+                            <p className="name">Victor Crest</p>
+                            <p className="age">26</p>
                         </div>
-                        <p class="location">London</p>
+                        <p className="job">Help Desk Operator</p>
+                        <p className="location">Washington</p>
                     </div>
                     <hr />
-                    <div class="user-stats flex-row">
-                        <div class="followers-container stats-container flex-column">
-                            <p class="stat-count">80K</p>
-                            <p class="stat-title">Followers</p>
+                    <div className="user-stats flex-row">
+                        <div className="followers-container stats-container flex-column">
+                            <p className="stat-count">Email</p>
+                            <p className="stat-title">atuny0@sohu.com</p>
                         </div>
-                        <div class="likes-container stats-container flex-column">
-                            <p class="stat-count">803K</p>
-                            <p class="stat-title">Likes</p>
+                        <div className="photos-container stats-container flex-column">
+                            <p className="stat-count">Address</p>
+                            <p className="stat-title">1745 T Street Southeast</p>
                         </div>
-                        <div class="photos-container stats-container flex-column">
-                            <p class="stat-count">1.4K</p>
-                            <p class="stat-title">Photos</p>
+                        <div className="likes-container stats-container flex-column">
+                            <p className="stat-count">Phone</p>
+                            <p className="stat-title">+63 791 675 8914</p>
                         </div>
                     </div>
                 </div>
